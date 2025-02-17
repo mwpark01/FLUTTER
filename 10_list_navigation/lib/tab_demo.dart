daa17003a35b8bae880d2348c7f9340fcdb93888
mwpark01 <hyminwoo0000@gmail.com>
2025-02-17 11:36:19 +0900

    IOS 스타일의 탭바 추가

--- 10_list_navigation/lib/tab_demo.dart
+++ 10_list_navigation/lib/tab_demo.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// IOS 스타일의 탭바
+// import 'package:flutter/cupertino.dart';
+
 class TabDemo extends StatelessWidget {
   const TabDemo({super.key});
 
@@ -33,3 +36,45 @@ class TabDemo extends StatelessWidget {
     );
   }
 }
+
+// IOS 스타일의 탭바
+
+// class TabDemo extends StatelessWidget {
+//   const TabDemo({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return CupertinoPageScaffold(
+//       navigationBar: CupertinoNavigationBar(
+//         middle: const Text('Tab Demo'),
+//         //trailing: ,  // 오른쪽에 추가적인 위젯 (선택 사항)
+//       ),
+//       child: CupertinoTabScaffold(
+//           tabBar: CupertinoTabBar(
+//             items: const <BottomNavigationBarItem>[
+//               BottomNavigationBarItem(
+//                 icon: Icon(CupertinoIcons.home),
+//                 label: 'Tab #1',
+//               ),
+//               BottomNavigationBarItem(
+//                 icon: Icon(CupertinoIcons.settings),
+//                 label: 'Tab #2',
+//               ),
+//             ],
+//           ),
+//           tabBuilder: (BuildContext context, int index) {
+//             return CupertinoTabView(
+//               builder: (BuildContext context) {
+//                 return Scaffold(
+//                   body: Center(
+//                     child: index == 0
+//                         ? const Text('Tab #1')
+//                         : const Text('Tab #2'),
+//                   ),
+//                 );
+//               },
+//             );
+//           }),
+//     );
+//   }
+// }
