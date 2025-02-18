cec20e13090a2f75bc2342ff8490e28e641748f8
mwpark01 <hyminwoo0000@gmail.com>
2025-02-18 10:17:02 +0900

    factory 추가, JSON 문자열 오류 처리 추가

--- data_json/lib/4_json_again.dart
+++ data_json/lib/4_json_again.dart
@@ -4,12 +4,16 @@ class User {
   final String name;
   final int age;
 
+  // 기본 생성자
   User({required this.name, required this.age});
 
   Map<String, dynamic> toMap() {
     return {'name': name, 'age': age};
   }
 
+  // factory 생성자
+  // 생성자를 통해 객체를 생성하는 대신, 객체를 생성하는 함수를 만들어 사용하는 방법
+  // 생성자를 통해 객체를 생성하는 것보다 더 많은 유연성을 제공
   factory User.fromJson(Map<String, dynamic> map) {
     return User(name: map['name'], age: map['age']);
   }
@@ -25,10 +29,14 @@ void main() {
   // JSON 문자열을 Map으로 변환 => Deserialize 역 직렬화
   String jsonString = '{"name": "김철수", "age": 30}';
 
-  Map<String, dynamic> userMap = jsonDecode(jsonString);
-  print(userMap);
+  try {
+    Map<String, dynamic> userMap = jsonDecode(jsonString);
+    print(userMap);
 
-  var user2 = User.fromJson(userMap);
-  print(user2.name);
-  print(user2.age);
+    var user2 = User.fromJson(userMap);
+    print(user2.name);
+    print(user2.age);
+  } catch (e) {
+    print('잘못된 JSON 문자열입니다.');
+  }
 }
